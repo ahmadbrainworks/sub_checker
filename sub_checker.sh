312a073489b7bcf07296ea518d064ba4427cb140
ahmadbrainworks <ahmadbrainworks@gmail.com>
2020-03-25 11:47:32 +0100

    added: saving file as .log and max timeout 15seconds

---
+++ sub_checker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while read line
+  do
+    name=$line
+echo -e " PAGE TILE: " && timeout 20   wget --quiet -O - $line \
+  | sed -n -e 's!.*<title>\(.*\)</title>.*!\1!p'
+
+echo -e "$line Response header" 
+   curl -I https://$name --connect-timeout 20
+ echo -e "$line DNS and Host records"
+ timeout 20    host $name  && nslookup $name
+ echo -e " ------------------------------------------------------------------------"   
+done < $1 >>  "results.txt"
+echo -e "saving file.."
+echo -e "filename" && date +%Y-%m-%d-%s.log
+mv results.txt  `date +"%Y-%m-%d-%s.log"`
